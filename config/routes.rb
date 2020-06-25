314f4ce428e49d8be51fd8b6f06cc769f9e3b067
Claire06140 <claire.marchyllie25@gmail.com>
2020-06-25 15:18:42 +0800

    seeds ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get "/cocktails", to: "cocktails#index", as: :cocktails
+  get "cocktails", to: "cocktails#index", as: :cocktails
 
-  # get "/cocktails/new", to: "cocktails#new", as: :new_cocktail
-  # post "/cocktails", to: "cocktails#create"
+  get "/cocktails/new", to: "cocktails#new", as: :new_cocktail
+  post "/cocktails", to: "cocktails#create"
 
-  # get "/cocktails/:id", to: "cocktails#show", as: :cocktail
+  get "/cocktails/:id", to: "cocktails#show", as: :cocktail
 
-resources :cocktails, only: [:index, :new, :create, :show]
+# resources :cocktails, only: [:index, :new, :create, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+ingredients = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)['drinks']
+ingredients.each { |ingredient| Ingredient.create(name: ingredient['strIngredient1'].downcase) }
+8.times do
+  data = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/random.php').read)['drinks'][0]
+  cocktail = Cocktail.create(name: data['strDrink'].downcase)
+  ingredients = []
+  ingredient_names = data.select { |key, _| key.to_s.match(/strIngredient/) }.values.compact
+  ingredient_names.each do |name|
+    result = Ingredient.find_by name: name.downcase
+    result = Ingredient.create(name: name.downcase) if result.nil?
+    ingredients << result
+  end
+  amounts = data.select { |key, _| key.to_s.match(/strMeasure/) }.values.compact
+  doses = amounts.zip(ingredients)
+  doses.each { |dose| Dose.create(cocktail: cocktail, ingredient: dose[1], description: dose[0].strip) }
+end
